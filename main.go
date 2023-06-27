623069ee1b8c05065f9f2e9fce7cd00d6e953e55
Hector Reyes Aleman <hreyes@mobiquityinc.com>
2023-06-27 12:21:33 +0200

    Add infoLogger and errorLogger

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
 
 func main() {
+	logFile, err := os.OpenFile("aws_cli_profiles_execution.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+
+	infoLogger := log.New(multiWriter, "[INFO] ", log.Ldate|log.Ltime)
+	errorLogger := log.New(multiWriter, "[ERROR] ", log.Ldate|log.Ltime)
+
 	accountProfiles := map[string]string{
 		"sandbox": "XXXXXXXXXXXX",
 		"dev":     "XXXXXXXXXXXX",
@@ -18,8 +30,8 @@ func main() {
 	}
 
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: aws-cli-profiles-execute [AWS CLI command]")
-		fmt.Println("Example: go run main.go \"aws s3 ls\"")
+		errorLogger.Println("Usage: aws-cli-profiles-execute [AWS CLI command]")
+		errorLogger.Println("Example: go run main.go \"aws s3 ls\"")
 		return
 	}
 
@@ -28,12 +40,12 @@ func main() {
 	// Iterate over the profiles
 	for profile, accountId := range accountProfiles {
 		os.Setenv("AWS_PROFILE", profile)
-		fmt.Println("Executing command for profile: " + profile)
+		infoLogger.Println("Executing command for profile: " + profile)
 		cmdArgs := strings.Fields(awsCommand)
 
 		// The first item in cmdArgs must be "aws"
 		if cmdArgs[0] != "aws" {
-			fmt.Println("Invalid command. The first item in the command must be \"aws\"")
+			errorLogger.Println("Invalid command. The first item in the command must be \"aws\"")
 			return
 		}
 
@@ -52,10 +64,10 @@ func main() {
 		output, err := cmd.CombinedOutput()
 
 		if err != nil {
-			fmt.Println(err)
+			errorLogger.Println(err)
 			continue
 		}
 
-		fmt.Printf("Output for profile %s (Account ID: %s):\n%s\n", profile, accountId, string(output))
+		infoLogger.Printf("Output for profile %s (Account ID: %s):\n%s\n", profile, accountId, string(output))
 	}
 }
